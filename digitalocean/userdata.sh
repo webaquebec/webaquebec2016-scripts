830d6cc4ab1c52badfa1e3244fe871b5dd5c601a
Paul Côté <github@paulcote.net>
2016-01-17 16:14:32 -0500

    Update scripts

--- digitalocean/userdata.sh
+++ digitalocean/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O /tmp/install.sh https://raw.githubusercontent.com/webaquebec/webaquebec2016-scripts/master/install.sh
+wget --no-check-certificate -O /tmp/install.sh https://raw.githubusercontent.com/webaquebec/webaquebec2016-scripts/master/install.sh
 chmod +x /tmp/install.sh
 /tmp/install.sh
